08cea5c1b94bb2d0f50128c834d80ab75a786f74
de-pdl <paudel.ayushp@gmail.com>
2026-04-30 01:16:56 +1000

    tweaked some display around

--- fish/config.fish
+++ fish/config.fish
@@ -8,11 +8,15 @@ set fish_greeting
 
 # echo "Welcome to Fish"
 if status is-interactive
-    fastfetch 
+    fastfetch
 end
-
 
 # Fish automatically sources files in ~/.config/fish/conf.d/
 # No need to manually source them here
 
 starship init fish | source
+
+# Per-machine config, not synced
+if test -f ~/.config/fish/config.local.fish
+    source ~/.config/fish/config.local.fish
+end
